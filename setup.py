0e44d6bfe66238e0a99e5b2640b08b8b721e8124
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-01 17:53:32 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#encoding: utf-8
+from setuptools import setup, find_packages
+from os import path
+setup(
+	name = "tentacle_pi",
+	version = "0.0.1.dev1",
+	description = "Tentacles for the Raspberry Pi!",
+	author = "Alexander Rüedlinger",
+	author_email = "a.rueedlinger@gmail.com",
+	license = "MIT"
+)
