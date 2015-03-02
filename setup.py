602b623ea94472c2da9f7e1ed0f74a365b216736
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-02 20:21:32 +0100

    fix setup.py warnings

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
 from distutils.core import setup, Extension
+from distutils.command import sdist as sdist_module
 import os
 
 DRIVERS = [
@@ -14,10 +15,10 @@ CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 def make_extensions(drivers):
 	exts = []
 	for (extension_name, extension_dir, extension_src_dir) in drivers:
-		ext_path = os.path.join(CURRENT_DIR, extension_dir)
-		src_path = os.path.join(ext_path, extension_src_dir)
-		c_files = filter(lambda x: x.endswith(".c"), src_path)
-		c_files = [ os.path.join(CURRENT_DIR, f) for f in c_files ]
+		src_path = os.path.join(extension_dir, extension_src_dir)
+		c_files = os.listdir(src_path)
+		c_files = filter(lambda x: x.endswith(".c"), c_files)
+		c_files = [ os.path.join(src_path, f) for f in c_files ]
 		exts.append((extension_name, c_files, src_path))
 	return exts
 	
@@ -27,20 +28,27 @@ def make_manifest_file(extensions):
 		[ f.write("graft %s\n" % src_path) for (_,_,src_path) in extensions]
 	
 
-extensions = make_extensions(DRIVERS)
 
-make_manifest_file(extensions)
-
-ext_modules = [ Extension(name, src_dir) for (name, src_dir,_) in extensions ] 
+class sdist(sdist_module.sdist):
+	
+	def run(self):
+		extensions = make_extensions(DRIVERS)
+		make_manifest_file(extensions)
+		sdist_module.sdist.run(self)
 
 setup(
 	name = "tentacle_pi",
-	version = "0.1.0.dev1",
+	version = "0.1.0",
 	description = "Tentacles for the Raspberry Pi!",
-	
-	ext_modules = ext_modules,
-	
+	url = "https://github.com/lexruee/tentacle_pi",
 	author = "Alexander Rüedlinger",
 	author_email = "a.rueedlinger@gmail.com",
+	
+	cmdclass = {
+		"sdist": sdist,
+	},
+	
+	ext_modules = [ Extension(name, src_dir) for (name, src_dir,_) in make_extensions(DRIVERS) ],
+	
 	license = "MIT"
 )
