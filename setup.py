7b260e399fa6f596c85c54af91f32ba84531f8ec
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-02 21:38:27 +0100

    make it pip installable :-)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class sdist(sdist_module.sdist):
 
 setup(
 	name = "tentacle_pi",
-	version = "0.1.0.dev3",
+	version = "0.1.0",
 	description = "Tentacles for the Raspberry Pi!",
 	url = "https://github.com/lexruee/tentacle_pi",
 	author = "Alexander Rüedlinger",
