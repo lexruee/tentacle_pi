f461180a9c1f5d578372baa6cc1e9deac3c27994
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-02 16:09:36 +0100

    update bmp180

--- bmp180/examples/test.py
+++ bmp180/examples/test.py
@@ -3,10 +3,10 @@ import time
 bmp = BMP180(0x77,"/dev/i2c-1")
 
 
-for x in range(0,5):
-        print "temperature: %s" % bmp.temperature()
+for x in range(0,100):
+        print "temperature: %0.1f" % bmp.temperature()
         print "pressure: %s" % bmp.pressure()
-        print "altitude: %s" % bmp.altitude()
+        print "altitude: %0.1f" % bmp.altitude()
         print
         time.sleep(2)
 
